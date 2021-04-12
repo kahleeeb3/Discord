7e3b7543df0e63920f7502e940f0ab2896aa07bf
Kahleeeb3 <powellcalebm@gmail.com>
2021-04-11 22:15:46 -0400

    new role menu

--- cogs/rolemenu/roles.py
+++ cogs/rolemenu/roles.py
@@ -3,11 +3,13 @@ from discord.ext import commands
 from modules import lists, menus
 from modules.json import json
 
-correct_message_id = 811657569515864124
-emoji_list = ['🖥','🫂','😂','🐶','🎬', '🎵', '🙌']
-role_names = ["CSC Stuff",'General', "Memes", "Pets","Movies","Music",'Commoners']
-class_list = ['chad','excellent','ross','Spongetard','joe']
-class_names = ['Complex, Anal Si','Electronics','E&M','Theory of Programming Languages','Ad Lab']
+"""
+"Complex": "chad",
+"Electronics": "excellent",
+"E&M": "ross",
+"Theory of Programming Languages": "Spongetard",
+"Ad Lab": "joe"
+"""
 
 class RoleReactions(commands.Cog):
     """Sends the role menu to the chat"""
@@ -15,26 +17,40 @@ class RoleReactions(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    #channel_id = 785343273190555648
-    #channel = self.client.get_channel(channel_id)
-
     @commands.command()
     async def rolemenu(self,ctx):
-        data = json.load('rolemenu')
-        title = data["menu"]["1"]["title"]
-        description = data["menu"]["1"]["description"]
-        embed = discord.Embed(title=f'{title}',description = description,color=discord.Color.red())
-
-        for role in data["menu"]["1"]["roles"]:
-            emoji = data["menu"]["1"]["roles"][role]["emoji"]
-            embed.add_field(name=f'{emoji}',value = f'{role}', inline=True)
-
+        # creates the embed
+        embed = menus.get_embed(1)
         menu = await ctx.send(embed=embed)
-        await menus.add_right_arrow(menu)
+        # add the reactions
+        await menus.add_emojis(menu, 1)
+        # change the role menu id
+        menus.change_id(menu.id)
+
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
 
+        # Gets the information from reaction
+        information = menus.get(payload)
+        user = information['user']
+        emoji= information['emoji']
+
+        # checks if input is from bot
+        if user.bot:
+            pass
+        else:
+            #check if reaction is ◀ or ▶
+            if emoji.name == '▶':
+                await menus.flip_right(self.client, payload)
+            """
+            channel = self.client.get_channel(payload.channel_id)
+            msg = await channel.fetch_message(payload.message_id)
+            embed = msg.embeds[0]
+            print(embed.title[0])
+            """
+        """
+        # gives the user the role
         async def give_role(user,role):
             guild_id = payload.guild_id
             guild = self.client.get_guild(guild_id)
@@ -44,10 +60,12 @@ class RoleReactions(commands.Cog):
             else:
                 await user.add_roles(role)
         
-
+        # Gets the information from reaction
         information = menus.get(payload)
         user = information['user']
         emoji= information['emoji']
+
+        # checks if input is from bot
         if user.bot:
             pass
         else:
@@ -56,7 +74,6 @@ class RoleReactions(commands.Cog):
                 #check if reaction is ◀ or ▶
                 if reaction.name == '▶':
                     await menus.flip_right(self.client, payload)
-
 
             #check if the reaction should trigger a role adding
                 try:
@@ -104,6 +121,6 @@ class RoleReactions(commands.Cog):
                         await take_role(user,role)
                     except:
                         pass
-
+    """
 def setup(client):
     client.add_cog(RoleReactions(client))
--- modules/menus.py
+++ modules/menus.py
@@ -1,24 +1,54 @@
 import discord
 from modules import lists
+from modules.json import json
 
-async def add_emojis(bot,payload,message):
-    emojis = []
-    for line in message.content.split():
-        if len(line) == 1 and line != ':':
-            emojis.append(line)
-        elif line.startswith('<'):
-            emojis.append(line)
 
-    #get the emoji objects
-    for emoji_name in emojis:
-        try:
-            await message.add_reaction(emoji_name)
-        except:
-            await message.add_reaction(emoji_name)
-    
-    #finally, add the right arrow
-    await add_right_arrow(message)
+#correct_message_id = data["message_id"]
 
+def get_embed(number:int):
+    # loads in the data
+    data = json.load('rolemenu')
+    # define the embed
+    title = data["menu"][f"{number}"]["title"]
+    description = data["menu"][f"{number}"]["description"]
+    embed = discord.Embed(title=f'{title}',description = description,color=discord.Color.red())
+
+    for role in data["menu"][f"{number}"]["roles"]:
+        emoji = data["menu"][f"{number}"]["roles"][role]["emoji"]
+        embed.add_field(name=f'{emoji}',value = f'{role}', inline=True)
+
+    return embed
+
+async def add_emojis(menu, number:int):
+    data = json.load('rolemenu')
+    for role in data["menu"][f"{number}"]["roles"]:
+        emoji = data["menu"][f"{number}"]["roles"][role]["emoji"]
+        await menu.add_reaction(emoji)
+
+    await menu.add_reaction('▶')
+
+def change_id(message_id):
+    data = json.load('rolemenu')
+    data["message_id"] = message_id
+    json.edit('rolemenu',data)
+
+async def flip_right(bot,payload):
+    # get menu number
+    channel = bot.get_channel(payload.channel_id)
+    msg = await channel.fetch_message(payload.message_id)
+    embed = msg.embeds[0]
+    current = int(embed.title[0])
+    try:
+        embed2 = get_embed(current + 1)
+        current = current + 1
+    except:
+        embed2 = get_embed(1)
+        current = 1
+    await msg.clear_reactions()
+    await msg.edit(embed = embed2)
+    await add_emojis(msg, current)
+
+"""These are all old"""
 def get(payload):
     user = payload.member
     message_id = payload.message_id
@@ -31,25 +61,6 @@ def match(a,b):
     else:
         return False
 
-async def flip_right(bot,payload):
-    #get menu information
-    channel = bot.get_channel(payload.channel_id)
-    message_id = payload.message_id
-    message = await channel.fetch_message(message_id)
-    #get current menu number
-    menu_number = int(message.content.split('\n')[0].split(' ')[1].replace(':',''))-1
-    # get the next menu content
-    menu_content = lists.to_string('rolemenus')
-    try:
-        menu_next = menu_content.split("END OF LIST")[menu_number+1]
-    except:
-        menu_next = menu_next = menu_content.split("END OF LIST")[0]
-    # change the menu
-    await message.edit(content = menu_next)
-    # remove all emojis and add new ones
-    await message.clear_reactions()
-    await add_emojis(bot,payload,message)
-    
 async def add_right_arrow(menu):
     await menu.add_reaction('▶')
 
